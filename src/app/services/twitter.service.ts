ffec8c2c3180fa0f1d2b0d38735464dae0223d64
Gabriel Rocha <gabrielrs7@outlook.com>
2022-08-01 01:09:49 -0300

    feat:changing api

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -10,8 +10,8 @@ export class TwitterService {
   constructor(private http: HttpClient) { }
 
   public getUserByUsername(): Observable<any>{
-    return this.http.get(`https://api.twitter.com/2/users/by?usernames=UrsoPolNE&user.fields=description%2Cprofile_image_url`,
-    {headers:{'Authorization': `Bearer AAAAAAAAAAAAAAAAAAAAACqefQEAAAAA9UA77HH4oId7122yloSgffO4A0Y%3D8ceaVvsLohtrPIZxk89oimi5XN6TEiJ6S1xJAuTgSkBhuu8SJj`}}
+    return this.http.get(`https://twitter-req.herokuapp.com/api/user?username=sommelierdebeta`
+    // {headers:{'Authorization': `Bearer AAAAAAAAAAAAAAAAAAAAACqefQEAAAAA9UA77HH4oId7122yloSgffO4A0Y%3D8ceaVvsLohtrPIZxk89oimi5XN6TEiJ6S1xJAuTgSkBhuu8SJj`}}
     )
   }
   // public getTwittesByUserId():Observable<any>{
